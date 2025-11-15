20917e1df33cb8aa8e032053ebb868a5f400d619
tims-exe <timonkjohn@gmail.com>
2025-11-15 22:56:52 +0530

    refactored logic for execute endpoint

--- api/src/routes/order.ts
+++ api/src/routes/order.ts
@@ -2,7 +2,8 @@ import { FastifyPluginAsync } from 'fastify';
 import type { PrismaClient } from 'db';
 import { Queue } from 'bullmq';
 import { Redis } from 'ioredis';
-import { handleOrderMessage } from '../services/orderHandler';
+import { handleOrder } from '../services/orderHandler';
+import { z } from 'zod';
 
 interface RouteType {
   prisma: PrismaClient;
@@ -10,15 +11,49 @@ interface RouteType {
   redisSub: Redis;
 }
 
+const OrderSchema = z.object({
+    tokenIn: z.string(),
+    tokenOut: z.string(),
+    amount: z.coerce.number(),
+    orderType: z.enum(['market', 'limit', 'sniper']),
+});
+
 const orderRoutes: FastifyPluginAsync<RouteType> = async (fastify, opts) => {
   const { prisma, orderQueue, redisSub } = opts;
 
-  fastify.get('/api/orders/execute', { websocket: true }, (connection, req) => {
-    connection.on('message', (rawMsg) => {
-      handleOrderMessage(rawMsg, connection, prisma, orderQueue, redisSub).catch(err => {
-        connection.send(JSON.stringify({ error: err.message }));
-        connection.close();
-      });
+  fastify.post('/api/orders/execute', async (request, response) => {
+    const parsedData = OrderSchema.safeParse(request.body)
+    
+    if (!parsedData.success) {
+      return response.code(400).send({
+        error: "Invalid order data"
+      })
+    }
+
+    const body = parsedData.data
+
+    const order = await prisma.orders.create({
+      data: {
+        tokenIn: body.tokenIn,
+        tokenOut: body.tokenOut,
+        amount: body.amount,
+        orderType: body.orderType,
+        status: 'pending',
+      },
+    });
+
+    return response.send({ 
+      orderId: order.id 
+    });
+  })
+
+
+  fastify.get('/api/orders/execute/:id', { websocket: true }, (connection, req) => {
+    const { id } = req.params as { id: string }
+
+    handleOrder(id, connection, prisma, orderQueue, redisSub).catch(err => {
+      connection.send(JSON.stringify({ error: err.message }));
+      connection.close();
     });
   });
 };
--- api/src/services/orderHandler.ts
+++ api/src/services/orderHandler.ts
@@ -2,57 +2,41 @@ import type WebSocket from 'ws';
 import type { PrismaClient } from 'db';
 import { Queue } from 'bullmq';
 import { Redis } from 'ioredis';
-import { z } from 'zod';
 
+interface OrderData {
+  orderId: string;
+  tokenIn: string;
+  tokenOut: string;
+  amount: number;
+  orderType: string;
+}
 
-const OrderSchema = z.object({
-    tokenIn: z.string(),
-    tokenOut: z.string(),
-    amount: z.coerce.number(),
-    orderType: z.enum(['market', 'limit', 'sniper']),
-});
-
-
-export async function handleOrderMessage(rawMsg: WebSocket.RawData, connection: WebSocket, prisma: PrismaClient, orderQueue: Queue, redisSub: Redis) {
-    let body;
-    // validate body
-    try {
-        body = JSON.parse(rawMsg.toString());
-    } catch {
-        connection.send(JSON.stringify({ error: 'Invalid payload' }));
-        connection.close();
-        return;
-    }
-
-    const parsed = OrderSchema.safeParse(body);
-    if (!parsed.success) {
-        connection.send(JSON.stringify({ error: 'Invalid order data' }));
-        connection.close();
-        return;
-    }
-
-    body = parsed.data;
-
-    // push to db
-    const order = await prisma.orders.create({
-        data: {
-            tokenIn: body.tokenIn,
-            tokenOut: body.tokenOut,
-            amount: body.amount,
-            orderType: body.orderType,
-            status: 'pending',
-        },
+export async function handleOrder(orderId: string, connection: WebSocket, prisma: PrismaClient, orderQueue: Queue, redisSub: Redis) {
+    const order = await prisma.orders.findUnique({
+        where: { id: orderId },
     });
+
+    if (!order) {
+        connection.send(JSON.stringify({ error: 'Order not found' }));
+        connection.close();
+        return;
+    }
 
     connection.send(
         JSON.stringify({
             orderId: order.id,
-            status: 'pending',
+            status: order.status,
             message: 'Order received and queued',
         })
     );
 
-    await orderQueue.add('execute-order', { orderId: order.id, ...body });
+    await orderQueue.add('execute-order', { 
+        orderId: order.id, 
+        tokenIn: order.tokenIn,
+        tokenOut: order.tokenOut,
+        amount: order.amount,
+        orderType: order.orderType,
+    });
 
     const channel = `order:${order.id}`;
     await redisSub.subscribe(channel);
@@ -62,6 +46,8 @@ export async function handleOrderMessage(rawMsg: WebSocket.RawData, connection: 
 
         const update = JSON.parse(msg);
         connection.send(msg);
+
+        console.log(`[${update.orderId}] => Status : ${update.status}`);
 
         if (update.status === 'confirmed' || update.status === 'failed') {
             redisSub.off('message', handleMessage);
